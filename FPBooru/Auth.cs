66916c6bc307892a428848319d2787e5312c6edd
Chloride Cull <chloride@devurandom.net>
2015-02-24 18:46:00 +0100

    Create auth scheme

---
+++ FPBooru/Auth.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace FPBooru
+{
+    class Auth
+    {
+        static string ValidateSessionCookie(byte[] cookie, MySqlConnection conn)
+        {
+            MySqlCommand cmd = new MySqlCommand("SELECT username FROM fpbooru.usrs WHERE session = \"@sess\"", conn);
+            cmd.Parameters["@sess"].Value = cookie;
+            MySqlDataReader red = cmd.ExecuteReader();
+            foreach (string user in red)
+            {
+                return user;
+            }
+            return null;
+        }
+
+        static byte[] GetSessionCookie(string user, MySqlConnection conn)
+        {
+
+        }
+
+        static byte[] ResetSessionCookie(string user, MySqlConnection conn)
+        {
+
+        }
+    }
+}
