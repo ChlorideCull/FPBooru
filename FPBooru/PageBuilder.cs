9dc3b3e39e092520b79fff9a0762d2f7555e2c41
Chloride Cull <chloride@devurandom.net>
2015-02-25 18:07:02 +0100

    actually add stuff to the pagebuilder

--- FPBooru/PageBuilder.cs
+++ FPBooru/PageBuilder.cs
@@ -1,11 +1,26 @@
 ﻿using System;
+using System.IO;
 
 namespace FPBooru
 {
     public class PageBuilder
     {
+        private string rawHeader;
+        private string rawBottom;
+
         public PageBuilder()
         {
+            rawHeader = File.ReadAllText("template/top.pphtml");
+            rawBottom = File.ReadAllText("template/bottom.pphtml");
+        }
+
+        public string GetHeader(string UserName, int UserID) {
+            string tmp = rawHeader;
+            if (UserName != null)
+                tmp.Replace("%_-USRSTATUS-_%", "<a class=\"noButton\" href=\"/user?id=" + UserID + "\">Hello, " + Username + "!</a>");
+            else
+                tmp.Replace("%_-USRSTATUS-_%", "<a class=\"alignRight\" onclick=\"doLogin()\">Login</a>");
+            return tmp;
         }
     }
 }
