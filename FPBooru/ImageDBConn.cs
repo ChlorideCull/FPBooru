2ebdbe52793e852138ea6629d6cf6f7aa2ea076b
Chloride Cull <chloride@devurandom.net>
2015-02-26 20:15:51 +0100

    insert the SQL querys

--- FPBooru/ImageDBConn.cs
+++ FPBooru/ImageDBConn.cs
@@ -5,14 +5,21 @@ namespace FPBooru
 {
     public static class ImageDBConn
     {
-        public string[][] GetImages(MySqlConnection conn, int page) {
-            MySqlCommand cmd = new MySqlCommand("This SQL is written down somewhere", conn); //TODO Get SQL for paged, all images from notes
+        public static string[][] GetImages(MySqlConnection conn, int page) {
+            MySqlCommand cmd = new MySqlCommand("SELECT id, imagepath_csv FROM fpbooru.images ORDER BY id DESC LIMIT " + (16 * page) + "," + (16 * (page + 1)) + ";", conn);
             MySqlDataReader red = cmd.ExecuteReader();
             //TODO: Need the SQL before making the foreach
         }
 
-        public string[][] GetImages(MySqlConnection conn, int page, string[] tags) {
-            MySqlCommand cmd = new MySqlCommand("This SQL is also written down somewhere", conn); //TODO Get SQL for images with tags in field from notes
+        public static string[][] GetImages(MySqlConnection conn, int page, string[] tags) {
+            //REGEX Basic: The CSV field always end with a comma. Regex would be tag1,|tag2,|tag3, etc.
+            string regex;
+            foreach (string tag in tags)
+            {
+                regex += tag + ",|";
+            }
+            regex = regex.Substring(0, regex.Length - 1);
+            MySqlCommand cmd = new MySqlCommand("SELECT id, imagepath_csv FROM fpbooru.images WHERE tagids_csv REGEXP '" + regex + "' ORDER BY id DESC LIMIT " + (16 * page) + "," + (16 * (page + 1)) + ";", conn);
             MySqlDataReader red = cmd.ExecuteReader();
             //TODO: Need the SQL before making the foreach
         }
