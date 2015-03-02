64f404c55c25d6b7fa3f4e94923cb28c14f7b7c1
Chloride Cull <chloride@devurandom.net>
2015-03-02 16:05:46 +0100

    fix grid path

--- FPBooru/PageBuilder.cs
+++ FPBooru/PageBuilder.cs
@@ -30,7 +30,7 @@ namespace FPBooru
 		public string GetImageGrid(Image[] images) {
 			string output = "";
 			foreach (Image img in images) {
-				output += "<a href=\"show?id=" + img.id + "\" class=\"pic\"><img src=\"static/thumbs/" + img.imagepaths[0] + ".png\" /></a>";
+				output += "<a href=\"/show/" + img.id + "\" class=\"pic\"><img src=\"static/thumbs/" + img.imagepaths[0] + ".png\" /></a>";
 			}
 		}
 	}
