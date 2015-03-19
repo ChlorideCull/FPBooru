2a4b375019c3a30767d9d04bcf1c5b9cb94296a8
Chloride Cull <chloride@devurandom.net>
2015-03-19 13:42:46 +0100

    fully implement auth

--- FPBooru/Auth.cs
+++ FPBooru/Auth.cs
@@ -11,6 +11,8 @@ namespace FPBooru
 {
 	static class Auth
 	{
+		static private Random rand = new Random();
+
 		public static string GetUserFromSessionCookie(string cookie, MySqlConnection conn)
 		{
 			MySqlCommand cmd = new MySqlCommand("SELECT username FROM fpbooru.usrs WHERE session = \"@sess\"", conn);
@@ -23,20 +25,40 @@ namespace FPBooru
 			return null;
 		}
 
-		public static string AuthenticateUser(string user, byte[] sha256password)
+		public static string AuthenticateUser(string user, byte[] sha256password, MySqlConnection conn)
 		{
-			throw NotImplementedException();
-			return GetSessionCookie(user);
+			string cookie = GetSessionCookie(user, conn);
+			if (cookie != null) {
+				return cookie;
+			} else {
+				return ResetSessionCookie(user, conn);
+			}
 		}
 
 		public static string GetSessionCookie(string user, MySqlConnection conn)
 		{
-			return "";
+			MySqlCommand cmd = new MySqlCommand("SELECT session FROM fpbooru.usrs WHERE username = \"@uname\"", conn);
+			cmd.Parameters["@uname"].Value = user;
+			MySqlDataReader red = cmd.ExecuteReader();
+			foreach (string sess in red)
+			{
+				return sess;
+			}
+			return null;
 		}
 
 		public static string ResetSessionCookie(string user, MySqlConnection conn)
 		{
-			return "";
+			byte[] sess = new byte[32];
+			rand.NextBytes(sess);
+			string cookie = System.Convert.ToBase64String(sess);
+
+			MySqlCommand cmd = new MySqlCommand("UPDATE fpbooru.usrs SET session = \"@sess\" WHERE username = \"@uname\"", conn);
+			cmd.Parameters["@uname"].Value = user;
+			cmd.Parameters["@sess"].Value = cookie;
+			cmd.ExecuteNonQuery();
+
+			return cookie;
 		}
 	}
 }
