84b812df812995b0d10ac297d094b4482c2d2e2f
Chloride Cull <chloride@devurandom.net>
2015-05-17 22:47:32 +0200

    start implementing plugin interface

---
+++ PluginInterface/Interface.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PluginInterface {
+	// MAIN PLUGIN INTERFACE
+	public interface IPlugin {
+		bool SetPluginManager(IPluginManager PlugMan);
+		PluginInfo GetInformation();
+
+		string LoadPage(string title);
+		UserPin[] LoadUserPins(string UserName);
+	}
+
+	// DATA DEFINITIONS
+	public interface IPluginManager {
+		void RegisterPage(string title, IPlugin plugin);
+		string GetUserAppData(string UserName, IPlugin plugin);
+		bool SetUserAppData(string UserName, IPlugin plugin, string AppData);
+		string GetAppData(IPlugin plugin);
+		bool SetAppData(IPlugin plugin, string AppData);
+	}
+
+	public struct UserPin {
+		public string ImageURL;
+		public string Title;
+		public string FlavorText;
+		public string Description;
+	}
+
+	public struct PluginInfo {
+		public string Name;
+		public string VersionString;
+		public string URL;
+	}
+}
+
